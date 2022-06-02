442f3a889ab68046eb7569b45c153b3ea0dd93ae
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-06-02 16:15:18 +0200

    Actualizo completamente la carpeta JS

--- js/script-jquery.js
+++ js/script-jquery.js
@@ -18,15 +18,26 @@ $(function(){
             $("#LabelApellidos").css("color","#000");
         })
     })
-})
+})  
 
 $("#tamaño").on('change',function(){
     console.log(this.value);
         $.ajax({
             type: "POST",
             url: "http://localhost:5000/checksize",
+            data:{
+                tamaño: this.value,
+                parametro1: "Disponible",
+                parametro2: "No disponible",
+            },
             success: function(resultado){
                 $("#resultado_tamaño").text(resultado)
             }
         })
+        
+    if($("#tamaño").val() === "S"){
+        $("#resultado_tamaño").css("color","#ff0000");
+    }else{
+        $("#resultado_tamaño").css("color","#006400");
+    }       
 })
